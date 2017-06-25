6c38ed0453677f908f54ce68e248f29810dd176e
Tay <pleaseforwardtothankyou@gmail.com>
2017-06-25 20:38:03 +0100

    Update InitialMenu.py

--- InitialMenu.py
+++ InitialMenu.py
@@ -1,14 +1,18 @@
-def menu(maze, *args):
+import maze
+import os
+def menu(maze):
     while True:
         print("* AMAZING MAZE *")
         print("* 1. Start     *")
         print("* 2. Quit      *")
         choice = int(input("Do you want to enter the maze?"))
         if choice == 1:
-            maze(*args)
+            os.system('maze.py')                                            #<=easier to use os commands to switch from program to program in python
+            os.system('main.py')
         elif choice == 2:
             print("You MUST enter the Maze. To exit next time, use the hidden option 3.")
-            maze(*args)
+            os.system('maze.py')
+            os.system('main.py')
         elif choice == 3:
             print("Fare thee well, O Maze traveller.")
             break

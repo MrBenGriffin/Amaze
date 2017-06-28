7aca583061570ab3c94a3fb292ef5d1711087648
Eddie <eddie@iwantyourtshirt.co.uk>
2017-06-28 17:42:42 +0100

    Made the width and height as user prompts when launching front.py. Will work on converting them to tkinter Entry widgets as a splash screen pre-maze.

--- front.py
+++ front.py
@@ -2,24 +2,43 @@ from tkinter import *
 from Maze.maze import Maze
 from Maze.cell import Cell
 from Bod.miner import Miner
+maze_width = 0
+while True:
+    try:
+        maze_width =  int(input('Maze Width?: '))
+    except ValueError:
+        print("That's not a number!")
+        continue
+    else:
+        print('thanks')
+        break
+maze_height = 0
+while True:
+    try:
+        maze_height = int(input('Maze Height?: '))
+    except ValueError:
+        print("That's not a number!")
+        continue
+    else:
+        print('thanks')
+        break
 
-maze_width = 120
-maze_height = 70
 cell_size = Cell.size
 
 root = Tk()
-
-canvas = Canvas(root, width=cell_size * (maze_width + 2), height=cell_size * (maze_height + 2),
+if maze_width > 0 and maze_height > 0:
+    canvas = Canvas(root, width=cell_size * (maze_width + 2), height=cell_size * (maze_height + 2),
                 bg='gray')
-canvas.grid(columns=1, rows=1)
+    canvas.grid(columns=1, rows=1)
 
-the_maze = Maze(maze_width, maze_height)
-the_miner = Miner()
-sys.setrecursionlimit(10000)
-the_miner.dig(the_maze.cell(0, 0))
-# Okay, so building 120x70 rooms takes the miner a few seconds. We aren't going to use this miner
-# Paint the maze itself. we will need to update rune doors at some point.
-# Either we should paint them as open/closed and then overlay, or just redraw everything.
-the_maze.tk_paint(canvas)
-
-root.mainloop()
+    the_maze = Maze(maze_width, maze_height)
+    the_miner = Miner()
+    sys.setrecursionlimit(10000)
+    the_miner.dig(the_maze.cell(0, 0))
+    # Okay, so building 120x70 rooms takes the miner a few seconds. We aren't going to use this miner
+    # Paint the maze itself. we will need to update rune doors at some point.
+    # Either we should paint them as open/closed and then overlay, or just redraw everything.
+    the_maze.tk_paint(canvas)
+    root.mainloop()
+else:
+    print('You need to set a width and a height')

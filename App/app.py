aa46e2446b72fc60275eb31303adddb7259890d2
MrBenGriffin <ben@redsnapper.net>
2017-06-29 21:27:15 +0100

    got front to use App class

--- App/app.py
+++ App/app.py
@@ -6,7 +6,7 @@ from App.config import Config
 
 class App(object):
     def __init__(self, tk_root):
-        self.root = root
+        self.root = tk_root
         self.maze_windows = []
         self.config_window = tk_root     # for the moment, we shall use root for config.
         self.config = Config(self.config_window, self.create_maze)
@@ -21,8 +21,13 @@ class App(object):
         the_maze.tk_paint()
         self.maze_windows.append(maze_window)
 
+    @staticmethod
+    def run():
+        sys.setrecursionlimit(10000)
+        the_root = Tk()
+        App(the_root)
+        the_root.mainloop()
+
+
 if __name__ == "__main__":
-    sys.setrecursionlimit(10000)
-    root = Tk()
-    app = App(root)
-    root.mainloop()
+    App.run()

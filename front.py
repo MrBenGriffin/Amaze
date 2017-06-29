fda4b87b31debf9e0abbcc81421f81ea06e021f4
Ben Griffin <ben@redsnapper.net>
2017-06-29 09:46:06 +0100

    pythonised the input validation function.

--- front.py
+++ front.py
@@ -4,26 +4,12 @@ from Maze.cell import Cell
 from Bod.miner import Miner
 cell_size = Cell.size
 
-#Initialise a window
+# Initialise a window
 root = Tk()
 
-"""Function to check what is being typed into entry is an intiger
- - We are checking letter by letter"""
-def testVal(inStr,i,acttyp):
-    #i is the index of the char string
-    #acttyp is the type of action
-    #inStr is the value
-    ind=int(i)
-    if acttyp == '1': #if the action is an insert
-        if not inStr[ind].isdigit(): #if the character is not a digit
-            return False #don't let it go in
-    return True
 
-#register the above function so it can be called from an Entry
-intChecker = root.register(testVal)
-
-#This function pulls the sizes as defined by the Entry boxes and calls the maze
-def getSize():
+# This function pulls the sizes as defined by the Entry boxes and calls the maze
+def get_size():
     maze_width = int(E1.get())  # grab the entry of textbox 1 as an integer
     maze_height = int(E2.get())  # grab the entry of textbox 2 as an integer
 
@@ -31,10 +17,10 @@ def getSize():
     root.destroy()
 
     # make the maze
-    createMaze(maze_width, maze_height)
+    create_maze(maze_width, maze_height)
 
-# Create the textboxes for width and height
-"""valid percent substitutions for validatecommand (from the Tk entry man page)
+# Create the text-boxes for width and height
+"""valid percent substitutions for validate command (from the Tk entry man page)
 # %d = Type of action (1=insert, 0=delete, -1 for others)
 # %i = index of char string to be inserted/deleted, or -1
 # %P = value of the entry if the edit is allowed
@@ -45,25 +31,29 @@ def getSize():
 #      (key, focusin, focusout, forced)
 # %W = the tk name of the widget"""
 
-label1 = Label( root, text="Maze Width")
-E1 = Entry(root, bd=3, validate="key", validatecommand=(intChecker, '%P','%i','%d')) #validate=key means it checks each letter as typed
-label2 = Label( root, text="Maze Height")
-E2 = Entry(root, bd=3, validate="key", validatecommand=(intChecker, '%P','%i','%d'))
+# This is a simple input validation that accepts numbers between 1 and 250.
+int_checker = root.register(lambda text, i, action: action != '1' or text[int(i)].isdigit() and 0 < int(text) < 250)
 
-#Create the submit button to call the getSize function
-submit = Button(root, text ="Submit", command = getSize)
+label1 = Label(root, text="Maze Width")    # validate=key means it checks each letter as typed
+E1 = Entry(root, bd=3, validate="key", validatecommand=(int_checker, '%P', '%i', '%d'))
+label2 = Label(root, text="Maze Height")
+E2 = Entry(root, bd=3, validate="key", validatecommand=(int_checker, '%P', '%i', '%d'))
 
-#Add all of the text boxes and stuff to the grid
+# Create the submit button to call the getSize function
+submit = Button(root, text="Submit", command=get_size)
+
+# Add all of the text boxes and stuff to the grid
 label1.grid()
 E1.grid()
 label2.grid()
 E2.grid()
 submit.grid()
 
-#Actually create the maze
-def createMaze(w, h):
+
+# Actually create the maze
+def create_maze(w, h):
     root = Tk()
-    #First check we are not making it with no size
+    # First check we are not making it with no size
     if w > 0 and h > 0:
         canvas = Canvas(root, width=cell_size * (w + 2), height=cell_size * (h + 2),
                     bg='gray')
